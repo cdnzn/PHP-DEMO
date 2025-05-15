965f119da53aa4cd614f17d9628a51a76ec949a2
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-15 13:24:34 +0800

    new

--- demo.php
+++ demo.php
@@ -50,6 +50,16 @@ for ($ctr=0; $ctr<=$num; $ctr++){
 
 require __DIR__ . '/vendor/autoload.php';
 use Cocur\Slugify\Slugify;
+use Ramsey\Uuid\Uuid;
 
 $slugify = new Slugify();
-echo $slugify->slugify("This is the new title.");
+echo $slugify->slugify("This is the new title.") . "\n";
+
+$uuid = Uuid::uuid4();
+echo $uuid->toString();
+
+$faker = Faker\Factory::create();
+echo $faker->name() . "\n";
+echo $faker->email() . "\n";
+echo $faker->phoneNumber() . "\n";
+echo $faker->text() . "\n";
